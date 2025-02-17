60da8da2fd24ddb80682c1dc91f2fb973b3decb4
laura <lauravtd44@gmail.com>
2025-02-16 23:14:39 -0500

    finished lab 4 part 3

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -6,7 +6,28 @@ import java.util.ArrayList;
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  */
-public class MusicOrganizer
+/*
+ * 27. couldnt solve it :(
+ * 28.  for(Track filename : tracks) {
+        }
+ * 29. boolean found = false;
+    while(found) {
+    if(the keys are in the next place) {
+    found = true;
+    }}
+*  31. int sum = 0;
+        int i = 1;
+
+        while (i <= 10) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("The sum of numbers from 1 to 10 is: " + sum);    
+     */
+
+
+public class MusicOrganizer 
 {
     // An ArrayList for storing the file names of music files.
     private ArrayList<String> files;
@@ -31,6 +52,18 @@ public class MusicOrganizer
         files.add(filename);
     }
     
+    public void multiplesOfFive(){
+     //exercise 30
+     int firstNumber=10;
+      int lastNumber=95;
+     int multiple=firstNumber;
+      
+     while (multiple<=lastNumber && multiple>=firstNumber){
+        System.out.println(multiple);
+         multiple=multiple+5;
+          
+       }}
+    
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -39,6 +72,33 @@ public class MusicOrganizer
     {
         return files.size();
     }
+    
+    public boolean isPrime(int n) { //exercise 33
+    if (n < 2) {
+        return false;
+    }
+
+    int i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+
+    return true;
+}
+    
+    public int sum(int a, int b) { //Exercise 32
+    int sum = 0;
+
+    while (a<=b) {
+        sum=sum+a;
+        a++;
+    }
+
+    return sum;
+}
     
     /**
      * List a file from the collection.
@@ -57,8 +117,25 @@ public class MusicOrganizer
      */
     public void listAllFiles()
     {
+        int position=0; //exercise 24
         for(String filename : files) {
-            System.out.println(filename);
+            position++;
+            System.out.println(position + ": " + filename);
+        }
+    }
+    
+    public void listMatching(String searchString)
+    {
+        boolean found=false;
+        for(String filename:files) {
+            if(filename.contains(searchString)) {
+                //A match.
+                System.out.println(filename);
+                found=true;
+            }
+            else if (!found) {
+                System.out.println("no file names matched the search");
+            }
         }
     }
     
